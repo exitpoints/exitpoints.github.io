134188ab915e02a18311373e0249a0c3390f1cce
Kavi_[IlzeBriede] <mailit2kavi@gmail.com>
2020-07-01 17:48:50 -0400

    Update sketch.js

--- assets/14july_poster/sketch.js
+++ assets/14july_poster/sketch.js
@@ -9,6 +9,8 @@ function draw() {
 
   textSize(130);
   text('14 JULY', windowWidth/2.5, windowHeight/2);
+  textSize(100);
+  text('start 8PM', windowWidth/2, windowHeight/2 + 40);
   
   let s = 'online audiovisual live performance';
   textSize(15);
